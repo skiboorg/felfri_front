fad78262fac7ac385c740b837e44f582bcc922d4
xxx <dd@dd.dd>
2024-04-08 20:30:28 +0300

    init

--- components/ItemCard.vue
+++ components/ItemCard.vue
@@ -12,6 +12,14 @@ defineProps(['product','category'])
     <img class="alt_img" :src="product.image_alt" alt="">
     <p class="small-card-title">{{product.name}}</p>
     <p class="small-card-price">{{product.price}} ₽</p>
+    <div class="badges">
+      <div v-if="product.is_new" class="badge is-new">Новинка</div>
+      <div v-if="product.is_popular" class="badge is-pop">Популярный</div>
+      <div v-if="product.is_in_stock" class="badge is-not-in-stock">Нет в наличии</div>
+      <div v-if="product.is_arrive" class="badge is-arrive">В пути</div>
+    </div>
+
+
 <!--    <router-link v-if="category" :to="`/catalog/${category.slug}/${product.slug}`">-->
 <!--      <Button  outlined rounded size="small" class="customBtn roundedBtn2 w-full" label="Подробнее о товаре"/>-->
 <!--    </router-link>-->
